1b8797882a9c521b2d61cdc2615a41391fa7156c
JACOB PRATT <isuandme@gmail.com>
2018-03-24 16:13:33 -0500

    have the url

--- crawler.py
+++ crawler.py
@@ -1,19 +1,14 @@
 from bs4 import BeautifulSoup
 import certifi
 import urllib3
+import _thread
+import time
 
 class Crawler:
 
-    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-    
-
-    """
-    url_list.append('https://www.reddit.com/r/Showerthoughts/comments/86pkzc/the_most_unrealistic_thing_about_spongebob_is/')
-    url_list.append('https://www.reddit.com/r/The_Donald/')
-    """
-
     def __init__(self):
         self.url_list = []
+        self.http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
     
     def add_url(self, url):
         self.url_list.append(url)
@@ -22,21 +17,32 @@ class Crawler:
         for txt in self.url_list:
             print(txt)
 
-    def get_soup(self, url_list):
-        url = url_list.pop()
-        veggies = http.request('GET', url)
+    def get_soup(self, url):
+        veggies = self.http.request('GET', url)
         soup = BeautifulSoup(veggies.data, 'lxml')
         return soup
 
-    def build_url_list(self, start_url):
-        soup = get_soup(self.url_list)
+    def build_url_list(self):
+        if len(self.url_list) is None:
+            return None
+        url = self.url_list.pop()
+        soup = self.get_soup(url)
+        url_len = len(url)
+        
         for link in soup.find_all('a'):
-            print(link.get('href'))
+            str = link.get('href')
+            try:
+                if len(str) > url_len and str[:url_len] == url and "comments" in str and "?sort" not in str:
+                    self.url_list.append(str)
+            except:
+                continue
 
+        
 
 crawl = Crawler()
-crawl.add_url('https://www.reddit.com/r/Showerthoughts/comments/86pkzc/the_most_unrealistic_thing_about_spongebob_is/')
-crawl.build_url_list(
+crawl.add_url('https://www.reddit.com/r/The_Donald/comments/5asj7o/announcement_you_know_we_are_winning_because_ctr/d9ixlqv/')
+crawl.build_url_list()
+crawl.print_url_list()
 #soup = crawl.get_soup(url_list)
 
 
