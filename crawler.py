f2c42bf5406ed58eb6b0fbbef529356397b5d3fe
JACOB PRATT <isuandme@gmail.com>
2018-03-24 00:14:02 -0500

    3/16/18/12:13AM - haven't updated in a while should probabaly see what I changed.

--- crawler.py
+++ crawler.py
@@ -1,3 +1,20 @@
 import scrapy
 
+class RedditMessageSpider(scrapy.Spider):
+    name = "message"
+    
+    def start_requests(self, url_string) :
+        urls = [url_string,]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+
+
+
+
+
+# EVERYTHING BELOW THIS IS A TEST #
+
+red = RedditMessageSpider()
+red.start_requests("http://quotes.toscrape.com/tag/humor/")
 
